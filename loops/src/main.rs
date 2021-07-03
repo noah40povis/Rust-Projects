edb309689484727b9d1d4372b64d682130495947
noah40povis <n.c.povis@gmail.com>
2021-07-02 23:47:02 -0400

    added a more concise version of while loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -8,3 +8,13 @@ fn main() {
         index += 1;
     }
 }
+
+// a more concise and faster version 
+
+// fn better_main() {
+//     let a = [10,20,30,40,50];
+
+//     for element in a.iter() {
+//         println!("the value is: {}", element);
+//     }
+// }
