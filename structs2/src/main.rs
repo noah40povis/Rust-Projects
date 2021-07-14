fead5beb22e4f87d75e120ae1e13d681cc07727e
noah40povis <n.c.povis@gmail.com>
2021-07-13 21:26:09 -0400

    adding debug annotation to print a structure

--- structs2/src/main.rs
+++ structs2/src/main.rs
@@ -1,13 +1,19 @@
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1,height1)
-    );
+// annotation for debugging information 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    let rect1 =Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:#?}", rect1); 
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
